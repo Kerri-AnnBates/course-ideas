df414906b8ac3a358d656c26da797346591dba52
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-08-13 14:05:49 -0400

    add partials and new requests

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -1,5 +1,6 @@
 package com.teamtreehouse.courses;
 
+import com.teamtreehouse.courses.model.CourseIdea;
 import com.teamtreehouse.courses.model.CourseIdeaDAO;
 import com.teamtreehouse.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
@@ -8,15 +9,17 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
             model.put("username", req.cookie("username"));
+
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -25,8 +28,26 @@ public class Main {
             String username = req.queryParams("username");
             res.cookie("username", username);
             model.put("username", username);
+            res.redirect("/");
 
-            return new ModelAndView(model, "sign-in.hbs");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        get("/ideas", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll());
+
+            return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String title = req.queryParams("title");
+            CourseIdea courseIdea = new CourseIdea(title, req.cookie("username"));
+            dao.add(courseIdea);
+            res.redirect("/ideas");
+            
+            return null;
         }, new HandlebarsTemplateEngine());
     }
 }
